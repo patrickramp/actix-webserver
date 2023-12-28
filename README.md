a479cec2e473370ce8ba61ddec35d0e6c6a11548
Patrick <patrick@tutamail.com>
2023-12-27 22:08:06 -0500

    Change from file to env variables for server config.

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-Simple webserver for serving static HTML pages using Actix famework in rust. Use config.toml to configure basic setting. 
+Simple webserver for serving static HTML pages and files using Actix framework in Rust.
 
-Application takes 1 argument to specify location of config.toml
+Server is configured by passing the following environment variables.: [default]
+  - BIND_TO= The IP address you wish the server to listen on. [0.0.0.0 (listens on all interfaces)] 
+  - PORT= Port you wish to use for the webserver. [8080]
+  - HTTP_DIR= Root directory you wish to serve public files from. [./public]
+  - DOMAIN= Public domain name for your server (example.com) [localhost]
+  - MOUNT= Primary web path to serve INDEX file ["/"]
+  - INDEX= Default file (in HTTP_DIR) to served when someone visits your domain [index.html]
+  - DOMAIN_TWO= Optional secondary domain name, [www.DOMAIN] (set as "" to disable)
+  - MOUNT_TWO= Optional secondary web path for DOMAIN_TWO ["MOUNT"]
+  - INDEX_TWO= Optional index file to serve at DOMAIN_TWO/MOUNT_TWO [INDEX]
 
--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,48 @@
+// [dev-dependencies]
 use actix_files::Files;
 use actix_web::{guard, App, HttpServer};
-use serde_derive::Deserialize;
-use std::{env, fs};
-
-// Top level struct to hold data from TOML file
-#[derive(Deserialize)]
-struct ConfigToml {
-    server_config: ServerConfig,
-}
-
-// Inner struct to hold data from [server_config] section of TOML file
-#[derive(Deserialize)]
-struct ServerConfig {
-    bind_address: String,
-    port: u16,
-    static_dir: String,
-    hostname1: String,
-    mount_one: String,
-    index_one: String,
-    hostname2: String,
-    mount_two: String,
-    index_two: String,
-}
-
-// Function to load the server configuration data from TOML file
-fn load_config(config_path: &str) -> ServerConfig {
-    // Deserialize the TOML data to top level struct
-    let config_toml: ConfigToml = toml::from_str(
-        &fs::read_to_string(config_path).expect("Unable to read from configuration file."),
-    )
-    .expect("Invalid TOML configuration file.");
-    // Return inner server_config data as ServerConfig struct
-    return config_toml.server_config;
-}
+use std::env;
 
 // Main Actix web server function
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // Collect command line arguments containing the path to the configuration file
-    let args: Vec<String> = env::args().collect();
-    // Load the configuration file specified by the command line arguments
-    let server_config: ServerConfig = load_config(&args[1]);
+    // Define server parameters from environment variables
+    let bind_to = env::var("BIND_TO").unwrap_or("0.0.0.0".to_string());
+    let port = env::var("PORT").unwrap_or("8080".to_string()).parse().unwrap();
+    let http_dir = env::var("HTTP_DIR").unwrap_or("./public".to_string());
+    let domain = env::var("DOMAIN").unwrap_or("localhost".to_string());
+    let mount = env::var("MOUNT").unwrap_or("/".to_string());
+    let index = env::var("INDEX").unwrap_or("index.html".to_string());
+    let domain_two = env::var("DOMAIN_TWO")
+        .unwrap_or(("www.".to_owned() + &domain).to_string());
+    let mount_two = env::var("MOUNT_TWO").unwrap_or(mount.to_string());
+    let index_two = env::var("INDEX_TWO").unwrap_or(index.to_string());
+
+    // Display server configuration to console
+    println!("Starting Actix Webserver at {}:{}", bind_to, port);
+    println!("Serving files from local directory \"{}\"", http_dir);
+    println!("Primary hostname is \"{}\" serving \"{}\" at \"{}\"", domain, index, mount);
+    println!("Secondary hostname is {} serving \"{}\" at \"{}\"", domain_two, index_two, mount_two);
 
     // Create an Actix web server with the specified configuration
     HttpServer::new(move || {
         App::new()
-            // Service for serving static files from configured directory
+            // Service for serving files from configured directory
             .service(
-                Files::new(&server_config.mount_one, &server_config.static_dir)
+                Files::new(&mount, &http_dir)
                     // Guard to restrict access to specified hostname (prevent hotlinks)
-                    .guard(guard::Host(&server_config.hostname1))
+                    .guard(guard::Host(&domain))
                     // Index file name
-                    .index_file(&server_config.index_one),
+                    .index_file(&index),
             )
-            // Redundant service to serve specified file from alternate hostname if needed
+            // Redundant service for optional secondary hostname(www.*), index file, and mount point
             .service(
-                Files::new(&server_config.mount_two, &server_config.static_dir)
-                    .guard(guard::Host(&server_config.hostname2))
-                    .index_file(&server_config.index_two),
+                Files::new(&mount_two, &http_dir)
+                    .guard(guard::Host(&domain_two))
+                    .index_file(&index_two),
             )
     })
-    .bind(((server_config.bind_address), (server_config.port)))
+    .bind((bind_to, port))
     .expect("Server unable to bind to specified address/port.")
     .run()
     .await

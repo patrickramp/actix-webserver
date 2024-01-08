9e68e41e769e696ba4eb4e36373553f1e85e3200
Patrick <patrick@tutamail.com>
2024-01-07 19:35:02 -0500

    Minor updates to env and output.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// [dev-dependencies]
+// [dependencies]
 use actix_files::Files;
 use actix_web::{guard, App, HttpServer};
 use std::env;
@@ -8,29 +8,22 @@ use std::env;
 async fn main() -> std::io::Result<()> {
     // Define server parameters from environment variables
     let bind_to = env::var("BIND_TO").unwrap_or("0.0.0.0".to_string());
-    let port = env::var("PORT")
-        .unwrap_or("8080".to_string())
-        .parse()
-        .unwrap();
+    let port = env::var("PORT").unwrap_or("8080".to_string()).parse::<u16>().unwrap();
     let http_dir = env::var("HTTP_DIR").unwrap_or("./public".to_string());
     let domain = env::var("DOMAIN").unwrap_or("localhost".to_string());
     let mount = env::var("MOUNT").unwrap_or("/".to_string());
     let index = env::var("INDEX").unwrap_or("index.html".to_string());
     let domain_two = env::var("DOMAIN_TWO").unwrap_or(("www.".to_owned() + &domain).to_string());
-    let mount_two = env::var("MOUNT_TWO").unwrap_or(mount.to_string());
-    let index_two = env::var("INDEX_TWO").unwrap_or(index.to_string());
+    let mount_two = env::var("MOUNT_TWO").unwrap_or(mount.clone());
+    let index_two = env::var("INDEX_TWO").unwrap_or(index.clone());
 
     // Display server configuration to console
-    println!("Starting Actix Webserver at {}:{}", bind_to, port);
-    println!("Serving files from local directory \"{}\"", http_dir);
-    println!(
-        "Primary hostname is \"{}\" serving \"{}\" at \"{}\"",
-        domain, index, mount
-    );
-    println!(
-        "Secondary hostname is {} serving \"{}\" at \"{}\"",
-        domain_two, index_two, mount_two
-    );
+    println!("Starting Actix Webserver...\n Listening on: {}:{}", bind_to, port);
+    println!(" Serving files from directory \"{}\"", http_dir);
+    println!(" Primary domain is \"{}\" serving \"{}\" at \"{}\"",
+        domain, index, mount);
+    println!(" Secondary domain is \"{}\" serving \"{}\" at \"{}\"",
+        domain_two, index_two, mount_two);
 
     // Create an Actix web server with the specified configuration
     HttpServer::new(move || {
@@ -50,7 +43,8 @@ async fn main() -> std::io::Result<()> {
                     .index_file(&index_two),
             )
     })
-    .workers(num_cpus::get())
+    // Number of worker threads to use per core
+    .workers(num_cpus::get()*2)
     .bind((bind_to, port))
     .expect("Server unable to bind to specified address/port.")
     .run()

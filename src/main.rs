75db50157275866ce6710f8acf15624c4f69dcef
Patrick <patrick@tutamail.com>
2024-01-20 18:19:07 -0500

    update deps. and minor formatting

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use std::env;
 async fn main() -> std::io::Result<()> {
     // Define server parameters from environment variables
     let bind_to = env::var("BIND_TO").unwrap_or("0.0.0.0".to_string());
-    let port = env::var("PORT").unwrap_or("8080".to_string()).parse::<u16>().unwrap();
+    let port = env::var("PORT").unwrap_or("8080".to_string());
     let http_dir = env::var("HTTP_DIR").unwrap_or("./public".to_string());
     let domain = env::var("DOMAIN").unwrap_or("localhost".to_string());
     let mount = env::var("MOUNT").unwrap_or("/".to_string());
@@ -18,12 +18,19 @@ async fn main() -> std::io::Result<()> {
     let index_two = env::var("INDEX_TWO").unwrap_or(index.clone());
 
     // Display server configuration to console
-    println!("Starting Actix Webserver...\n Listening on: {}:{}", bind_to, port);
+    println!(
+        "Starting Actix Webserver...\n Listening on: {}:{}",
+        bind_to, port
+    );
     println!(" Serving files from directory \"{}\"", http_dir);
-    println!(" Primary domain is \"{}\" serving \"{}\" at \"{}\"",
-        domain, index, mount);
-    println!(" Secondary domain is \"{}\" serving \"{}\" at \"{}\"",
-        domain_two, index_two, mount_two);
+    println!(
+        " Primary domain is \"{}\" serving \"{}\" at \"{}\"",
+        domain, index, mount
+    );
+    println!(
+        " Secondary domain is \"{}\" serving \"{}\" at \"{}\"",
+        domain_two, index_two, mount_two
+    );
 
     // Create an Actix web server with the specified configuration
     HttpServer::new(move || {
@@ -36,7 +43,8 @@ async fn main() -> std::io::Result<()> {
                     // Index file name
                     .index_file(&index),
             )
-            // Redundant service for optional secondary hostname(www.*), index file, and mount point
+            // Redundant service for optional secondary hostname(www.*), index file,
+            // and mount point
             .service(
                 Files::new(&mount_two, &http_dir)
                     .guard(guard::Host(&domain_two))
@@ -44,8 +52,8 @@ async fn main() -> std::io::Result<()> {
             )
     })
     // Number of worker threads to use per core
-    .workers(num_cpus::get()*2)
-    .bind((bind_to, port))
+    .workers(num_cpus::get() * 2)
+    .bind(format!("{}:{}", bind_to, port))
     .expect("Server unable to bind to specified address/port.")
     .run()
     .await

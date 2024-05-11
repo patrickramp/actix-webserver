a9c06cf6cb7c37106b7d72827fec52638a3a5e82
Patrick <patrick@tutamail.com>
2024-05-11 16:00:30 -0400

    Updated env var handeling and workers per cpu

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,15 @@ use std::env;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // Define server parameters from environment variables
-    let bind_to = env::var("BIND_TO").unwrap_or("0.0.0.0".to_string());
-    let port = env::var("PORT").unwrap_or("8080".to_string());
-    let http_dir = env::var("HTTP_DIR").unwrap_or("./public".to_string());
-    let domain = env::var("DOMAIN").unwrap_or("localhost".to_string());
-    let mount = env::var("MOUNT").unwrap_or("/".to_string());
-    let index = env::var("INDEX").unwrap_or("index.html".to_string());
-    let domain_two = env::var("DOMAIN_TWO").unwrap_or(("www.".to_owned() + &domain).to_string());
-    let mount_two = env::var("MOUNT_TWO").unwrap_or(mount.clone());
-    let index_two = env::var("INDEX_TWO").unwrap_or(index.clone());
+    let bind_to = env::var("BIND_TO").unwrap_or_else(|_| "0.0.0.0".to_string());
+    let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
+    let http_dir = env::var("HTTP_DIR").unwrap_or_else(|_| "./public".to_string());
+    let domain = env::var("DOMAIN").unwrap_or_else(|_| "localhost".to_string());
+    let mount = env::var("MOUNT").unwrap_or_else(|_| "/".to_string());
+    let index = env::var("INDEX").unwrap_or_else(|_| "index.html".to_string());
+    let domain_two = env::var("DOMAIN_TWO").unwrap_or_else(|_| format!("www.{}", domain.clone()));
+    let mount_two = env::var("MOUNT_TWO").unwrap_or_else(|_| mount.clone());
+    let index_two = env::var("INDEX_TWO").unwrap_or_else(|_| index.clone());
 
     // Display server configuration to console
     println!(
@@ -38,21 +38,21 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(
                 Files::new(&mount, &http_dir)
-                    // Guard to restrict access to specified hostname (prevent hotlinks)
+                    // Guard to restrict access to specified hostname
                     .guard(guard::Host(&domain))
                     // Index file name
                     .index_file(&index),
             )
-            // Redundant service for optional secondary hostname(www.*), index file,
-            // and mount point
+            // Redundant service for optional secondary hostname(www.*),
+            // index file, and mount point
             .service(
                 Files::new(&mount_two, &http_dir)
                     .guard(guard::Host(&domain_two))
                     .index_file(&index_two),
             )
     })
-    // Number of worker threads to use per core
-    .workers(num_cpus::get() * 2)
+    // Set number of worker threads to number of cores
+    .workers(num_cpus::get())
     .bind(format!("{}:{}", bind_to, port))
     .expect("Server unable to bind to specified address/port.")
     .run()
